d6e05c246b02edce3d5d2a5399c29e975cdf8cf4
Sondre Sparby Boge <sondreboge@hotmail.com>
2020-08-27 12:45:13 +0200

    Updates

---
+++ precache-manifest.bd1a7e00faab100003ef4342f520ffb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161d4132d5c7cb0a742b180aa7d2f2b3",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "27af94c87951420deceb",
+    "url": "/game-of-life/static/css/main.073d2fe0.chunk.css"
+  },
+  {
+    "revision": "ab30e0621471f7e95bbc",
+    "url": "/game-of-life/static/js/2.86e3d615.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.86e3d615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27af94c87951420deceb",
+    "url": "/game-of-life/static/js/main.04ff9f3c.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
--- src/utils/ArrayUtil.ts
+++
@@ -1,4 +0,0 @@
-const arrayFromTo = (width: number, height: number) =>
-  Array.from(Array(width), () => new Array(height).fill(false));
-
-export default arrayFromTo;
